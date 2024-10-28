ac18ddd4a1710c4822d36291296607fefa1e3a83
busappagarisushma <sushmabusappagari@gmail.com>
2024-10-28 19:40:19 +0530

    creating adminMapper

---
+++ src/main/java/com/jsp/pharmacy/mapper/AdminMapper.java
@@ -0,0 +1,28 @@
+package com.jsp.pharmacy.mapper;
+
+import com.jsp.pharmacy.entity.Admin;
+import com.jsp.pharmacy.requestdto.AdminRequest;
+import com.jsp.pharmacy.responsedto.AdminResponse;
+
+public class AdminMapper {
+
+	public Admin mapToUser(AdminRequest request,Admin admin) {
+
+		admin.setName(request.getName());
+		admin.setLicenceNo(request.getLicenceNo());;
+		admin.setGstNo(request.getGstNo());
+
+		return admin;
+	}
+
+	public AdminResponse mapToUserResponse(Admin user) {
+
+		AdminResponse response = new AdminResponse();
+		response.setId(response.getId());
+		response.setName(response.getName());
+		response.setLicenceNo(response.getLicenceNo());
+		response.setGstNo(response.getGstNo());
+
+		return response;
+	}
+}
