da7a19a922ea72bc875ef852e31a37929960eaeb
busappagarisushma <sushmabusappagari@gmail.com>
2024-10-28 17:28:09 +0530

    adding dependencys in controller and service classes

--- src/main/java/com/jsp/pharmacy/controller/AdminController.java
+++ src/main/java/com/jsp/pharmacy/controller/AdminController.java
@@ -1,5 +1,15 @@
 package com.jsp.pharmacy.controller;
 
+import com.jsp.pharmacy.service.AdminService;
+
 public class AdminController {
 
+	private final AdminService adminService;
+
+	public AdminController(AdminService adminService) {
+		super();
+		this.adminService = adminService;
+	}
+	
+	
 }
--- src/main/java/com/jsp/pharmacy/service/AdminService.java
+++ src/main/java/com/jsp/pharmacy/service/AdminService.java
@@ -1,5 +1,15 @@
 package com.jsp.pharmacy.service;
 
+import com.jsp.pharmacy.repo.AdminRepository;
+
 public class AdminService {
 
+	private final AdminRepository adminRepository;
+
+	public AdminService(AdminRepository adminRepository) {
+		super();
+		this.adminRepository = adminRepository;
+	}
+	
+	
 }
