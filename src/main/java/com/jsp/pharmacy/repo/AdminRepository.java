9fc2cb68d5843fb7adf35d3d96928866233b9075
busappagarisushma <sushmabusappagari@gmail.com>
2024-10-28 17:11:40 +0530

    adding admin repository

---
+++ src/main/java/com/jsp/pharmacy/repo/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.jsp.pharmacy.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.pharmacy.entity.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, String>{
+
+
+}
