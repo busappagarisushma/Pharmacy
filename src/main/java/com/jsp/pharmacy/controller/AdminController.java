fc73dd2246ab39fb12941707bc4b6eccbf6de85d
busappagarisushma <sushmabusappagari@gmail.com>
2024-10-28 19:20:10 +0530

    adding appResponseBuilder

--- src/main/java/com/jsp/pharmacy/controller/AdminController.java
+++ src/main/java/com/jsp/pharmacy/controller/AdminController.java
@@ -1,14 +1,23 @@
 package com.jsp.pharmacy.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.jsp.pharmacy.entity.Admin;
 import com.jsp.pharmacy.service.AdminService;
+import com.jsp.pharmacy.util.AppResponseBuilder;
+import com.jsp.pharmacy.util.ResponseStructure;
 
 public class AdminController {
 
 	private final AdminService adminService;
+	private final AppResponseBuilder appResponseBuilder;
 
-	public AdminController(AdminService adminService) {
+	public AdminController(AdminService adminService,AppResponseBuilder appResponseBuilder) {
 		super();
 		this.adminService = adminService;
+		this.appResponseBuilder = appResponseBuilder;
 	}
 	
 	
--- src/main/java/com/jsp/pharmacy/service/AdminService.java
+++ src/main/java/com/jsp/pharmacy/service/AdminService.java
@@ -1,6 +1,10 @@
 package com.jsp.pharmacy.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.jsp.pharmacy.entity.Admin;
 import com.jsp.pharmacy.repo.AdminRepository;
+import com.jsp.pharmacy.util.ResponseStructure;
 
 public class AdminService {
 
@@ -9,6 +13,11 @@ public class AdminService {
 	public AdminService(AdminRepository adminRepository) {
 		super();
 		this.adminRepository = adminRepository;
+	}
+
+	 public ResponseEntity<ResponseStructure<Admin>> getAdminById(String adminId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
