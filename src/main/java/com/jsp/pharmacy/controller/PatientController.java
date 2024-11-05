16d24f9bc46a2e436854af72cc49f7b43aaefb42
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-05 06:48:00 +0530

    creating controller,service,repository classes

---
+++ src/main/java/com/jsp/pharmacy/controller/PatientController.java
@@ -0,0 +1,8 @@
+package com.jsp.pharmacy.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+
+}
---
+++ src/main/java/com/jsp/pharmacy/service/PatientService.java
@@ -0,0 +1,8 @@
+package com.jsp.pharmacy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+
+}
